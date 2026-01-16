92d727776ee0b5b9cf13ae8435b3a677c8c5df34
SaklenManjire <saklenmanjire032@gmail.com>
2026-01-16 15:56:34 +0530

    first practical added

---
+++ ClassActivity/activity2.js
@@ -0,0 +1,13 @@
+console.log("Activity No 2");
+document.writeln("<h1>Activity No.2</h1>");
+let a=15;
+
+if(a%2==0)
+{
+    console.log(a+" Number is Even");
+    document.writeln("<h2>"+a+" Number is Even</h2>");
+}else{
+     console.log(a+" Number is Odd");
+    document.writeln("<h2>"+a+" Number is Odd</h2>");
+     
+}
---
+++ LabExperiment/exp1.js
@@ -0,0 +1,20 @@
+//variables 
+// const
+const a=10;
+
+//cant change const value 
+// a=20;
+console.log(a);
+
+//let
+//let cant redeclare due to scope
+let b=20;
+b=30;
+console.log(b);
+
+var ab=20;
+//var can reaasign and redeclare due to scope
+// var ab=30;
+ ab=30;
+console.log(ab);
+
